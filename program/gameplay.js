6d900350212fdcf69a8434274fa19dfa89bccdb7
Elinia <asdfjhc@gmail.com>
2018-01-05 18:34:48 +0900

    GUI ing

--- program/gameplay.js
+++ program/gameplay.js
@@ -2,56 +2,18 @@ NimGame = (function() {
 	var game_status;
 	const default_config = {
 		"player1": {
-			type: "player"
+			"type": "player"
 		},
 		"player2": {
-			type: "ai"
+			"type": "ai"
 		},
-		"stones": [-1, -1, -1],
-		"offensive": "player1"
-	};
-
-	var AI = function(stones) {
-		var xor = 0;
-		var action_pile = 0;
-		var number_of_most_stones = 0;
-		var action_pile_stones = 0;
-		for (var i = 0; i < stones.length; i++) {
-			xor ^= stones[i];
-			if (stones[i] > number_of_most_stones) {
-				action_pile = i;
-				number_of_most_stones = stones[i];
-			}
-		}
-		var action;
-		if (xor === 0) {
-			action_pile_stones = Math.floor(Math.random()*number_of_most_stones);
-			action = {
-				"pile_index": action_pile,
-				"stones": action_pile_stones,
-				"message": "Uhh... I'm not sure if I can win."
-			}
-		} else {
-			for (var i = 0; i < stones.length; i++) {
-				if ((stones[i] ^ xor) < stones[i]) {
-					action_pile = i;
-					action_pile_stones = stones[i] ^ xor;
-					break;
-				}
-			}
-			action = {
-				"pile_index": action_pile,
-				"stones": action_pile_stones,
-				"message": "I think I'm the winner :D"
-			}
-		}
-		return action;
+		"stones": [-1, -1, -1]
 	};
 
 	var get_action = function(player) {
 		return new Promise(function(resolve, reject) {
 			if (game_status[player]["type"] === "ai") {
-				resolve(AI(game_status["stones"]));
+				resolve(MyAI.action(game_status["stones"]));
 			} else if (game_status[player]["type"] === "player") {
 				var button = document.getElementById('button');
 				var pile_index = document.getElementById('pile_index');
@@ -67,39 +29,50 @@ NimGame = (function() {
 		});
 	};
 
+	var next_turn = function() {
+		if (game_status["turn"] === "player1") {
+			turn("player2");
+		} else if (game_status["turn"] === "player2") {
+			turn("player1");
+		} else {
+			throw "turn bug";
+		}
+	};
+
+	var game_finished = function() {
+		for (var i = 0; i < game_status["stones"].length; i++) {
+			if (game_status["stones"][i] > 0) {
+				return false;
+			}
+		}
+		return true;
+	};
+
 	var turn = function(player) {
+		game_status["turn"] = player;
+		console.log(player + "'s turn");
+		console.log(game_status["stones"]);
 		// draw();
 		if (game_status[player] === undefined) {
 			throw "game error";
 		} else {
-			console.log(player + "'s turn");
-			console.log(game_status["stones"]);
 			get_action(player).then(function(action) {
 				if (game_status["stones"][action["pile_index"]] === undefined) {
-					throw "pile index undefined";
+					throw "pile index out of range";
 				}
-				if (game_status["stones"][action["pile_index"]] < action["stones"]) {
-					throw "stones larger than expected";
+				if (action["stones"] >= game_status["stones"][action["pile_index"]] || action["stones"] < 0) {
+					throw "stones out of range";
 				}
+
 				game_status["stones"][action["pile_index"]] = action["stones"];
-				console.log(action["message"]);
-				var finished = true;
-				for (var i = 0; i < game_status["stones"].length; i++) {
-					if (game_status["stones"][i] > 0) {
-						finished = false;
-					}
-				}
-				if (finished) {
+				console.log(player + ": " + action["message"]);
+				
+				if (game_finished()) {
 					game_status["winner"] = player;
 					console.log("winner is " + game_status["winner"] + "!");
 					return;
 				} else {
-					if (game_status["turn"] === "player1") {
-						game_status["turn"] = "player2";
-					} else if (game_status["turn"] === "player2") {
-						game_status["turn"] = "player1";
-					}
-					turn(game_status["turn"]);
+					next_turn();
 				}
 			});
 		}
@@ -107,8 +80,12 @@ NimGame = (function() {
 	};
 
 	return {
-		init: function(config) {
-			
+		init: function() {
+			UI.gameConfig().then(function(config) {
+				NimGame.start(config);
+			});
+		},
+		start: function(config) {
 			game_status = default_config;
 			if (config !== undefined) {
 				for (var key in default_config) {
@@ -117,23 +94,27 @@ NimGame = (function() {
 					}
 				}
 			}
-			game_status["turn"] = game_status["offensive"];
 			for (var i = 0; i < game_status["stones"].length; i++) {
 				if (game_status["stones"][i] === -1) {
 					game_status["stones"][i] = Math.floor(Math.random()*10+1)
 				}
 			}
-			
 			console.log(game_status);
-			
-		},
-		start: function() {
-			turn(game_status["turn"]);
-			// drawfinish();
+
+			turn("player1");
 		}
 	};
 
 	
 })();
 NimGame.init();
-NimGame.start();
+/*NimGame.start({
+	"player1": {
+		"type": "player"
+	},
+	"player2": {
+		"type": "ai"
+	},
+	"stones": [-1, -1, -1],
+	"offensive": "player1"
+});*/
